702425e5295d58c6ffe54d5a648dfc463b4bddf3
surya <suryaprakashnaidu77@gmail.com>
2026-07-08 16:33:20 +0530

    Allow Vercel frontend origin

--- src/main/java/com/shortly/config/CorsConfig.java
+++ src/main/java/com/shortly/config/CorsConfig.java
@@ -21,7 +21,8 @@ public class CorsConfig {
                 "http://localhost:5176",
                 "http://localhost:5175",
                 "http://localhost:5174",
-                "http://localhost:5173"
+                "http://localhost:5173",
+                "https://shortly-frontend-gamma.vercel.app"
         ));
 
         configuration.setAllowedMethods(List.of(
